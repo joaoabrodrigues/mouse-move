cc571d1cd63d5e6f78072397ad900be1be64fb60
João Antônio Bezerra Rodrigues <joaoabrodrigues@outlook.com>
2024-01-12 11:12:27 -0300

    Create mouse-move.py

---
+++ mouse-move.py
@@ -0,0 +1,22 @@
+import pyautogui
+import time
+from random import randint
+
+pyautogui.FAILSAFE = false
+
+count = 1
+
+while(True) :
+  x, y = pyautogui.position()
+
+  x += randint(-3, 3)
+  y += randint(-3, 3)
+
+  pyautogui.moveTo(x, y)
+
+  print(f"Mouse moved! {count} times - Current datetime: {time.strftime('%x - %X')}")
+
+  count += 1
+
+  time.sleep(60)
+  
