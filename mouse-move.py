62125e71994d68f2bdab0ab1cab9cdc531d09f8e
João Antônio Bezerra Rodrigues <joaoabrodrigues@outlook.com>
2024-01-12 11:14:05 -0300

    Update mouse-move.py

--- mouse-move.py
+++ mouse-move.py
@@ -2,21 +2,21 @@ import pyautogui
 import time
 from random import randint
 
-pyautogui.FAILSAFE = false
+pyautogui.FAILSAFE = False
 
 count = 1
 
 while(True) :
-  x, y = pyautogui.position()
+    x, y = pyautogui.position()
 
-  x += randint(-3, 3)
-  y += randint(-3, 3)
+    x += randint(-3, 3)
+    y += randint(-3, 3)
 
-  pyautogui.moveTo(x, y)
+    pyautogui.moveTo(x, y)
 
-  print(f"Mouse moved! {count} times - Current datetime: {time.strftime('%x - %X')}")
+    print(f"Mouse moved! {count} times - Current datetime: {time.strftime('%x - %X')}")
 
-  count += 1
+    count += 1
 
-  time.sleep(60)
+    time.sleep(60)
   
